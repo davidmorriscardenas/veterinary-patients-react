bf562dd685b9e381590a9df852e4faa05c6c95d4
Morris Cardenas <david.morris.cardenas@hotmail.com>
2019-06-25 14:41:09 -0500

    morris changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './bootstrap.min.css';
+import Header from './components/Header.js';
+import NuevaCita from './components/NuevaCita.js';
 
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+class App extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <div className="container">
+        <Header
+          titulo='Administrador Pacientes Veterinaria'
+        />
+        <div className="row">
+          <div className="col-md-10 mx-auto">
+            <NuevaCita />
+          </div>
+        </div>
+      </div>
+     );
+  }
 }
-
+ 
 export default App;
